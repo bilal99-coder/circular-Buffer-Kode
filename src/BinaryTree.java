10bfc0ded993c6b667d4a1fc524c710709553042
Bilal Sahli <Bilalsahli1999@gmail.com>
2021-12-12 22:30:46 +0100

    Added new class BinaryTreeNode with two methods addLeftChild and addRightChild

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,3 +1,24 @@
 public class BinaryTree {
 
+    class BinaryTreeNode{
+        char value;
+        BinaryTreeNode left_child;
+        BinaryTreeNode right_child;
+
+
+        public BinaryTreeNode(char value){
+            this.value = value;
+            this.left_child = null;
+            this.right_child = null;
+        }
+
+        void addLeftChild(char value){
+            this.left_child = new BinaryTreeNode(value);
+        }
+
+        void addRightChild(char value){
+            this.right_child = new BinaryTreeNode(value);
+        }
+
+    }
 }
